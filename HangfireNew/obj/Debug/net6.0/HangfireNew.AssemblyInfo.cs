492139c9d57b5cf1b0840cedd040ac3d36d98d6b
DawoodAshraf22 <125259048+DawoodAshraf22@users.noreply.github.com>
2025-12-03 12:19:26 +0500

    Changes

--- HangfireNew/obj/Debug/net6.0/HangfireNew.AssemblyInfo.cs
+++ HangfireNew/obj/Debug/net6.0/HangfireNew.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HangfireNew")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53c222d1b70e8608cc2406e2c39260b35db9c22d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a50e40ddc4cdfae9d3f9f2aa93a9f032ade8efb")]
 [assembly: System.Reflection.AssemblyProductAttribute("HangfireNew")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HangfireNew")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
